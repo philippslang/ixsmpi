ad4c5964c611fe6d1651f4dd3a2b2c4f7841166e
Philipp Lang <PLang@slb.com>
2016-11-29 13:12:27 +0000

    discarded stringstream method and proceeded with iterator and dedicated containers

--- main.cpp
+++ main.cpp
@@ -1,55 +1,121 @@
 #include <iostream>
 #include <vector>
 
+
 template<typename T>
 struct BufferTraits
 {
-  typedef std::vector<T> BType;
+  typedef std::vector<T> BCType;
+  typedef typename BCType::const_iterator BCIterator;
 }; 
+
+
+struct OBufferIts
+{
+  BufferTraits<int>::BCIterator bi_int;
+};
+
 
 template<typename O>
 struct OBuffer
 {
-  size_t o_count;
-  typename BufferTraits<int>::BType b_int;
+  typedef std::vector<size_t> BCSizes;
+
+  BCSizes bc_sizes;
+  typename BufferTraits<int>::BCType bc_int;
+  mutable OBufferIts its;
 };
 
 
 namespace traits
-{
+{  
   template<typename T, typename B>
   struct access{};
   
   template<typename B>
   struct access<int, B>
   {
-    static typename BufferTraits<int>::BType& buffer(B &b)
+    static typename BufferTraits<int>::BCType& buffer(B &b)
     {
-      return b.b_int;
+      return b.bc_int;
+    }
+    static const typename BufferTraits<int>::BCType& buffer(const B &b)
+    {
+      return b.bc_int;
+    }
+    static typename BufferTraits<int>::BCIterator& buffer_iterator(const B &b)
+    {
+      return b.its.bi_int;
     }
   };
 }
 
+
 template<typename T, typename B>
-typename BufferTraits<T>::BType& buffer(B &b)
+typename BufferTraits<T>::BCType& buffer(B &b)
 {
-  return traits::access<T,B>::buffer(b);
+  return traits::access<T, B>::buffer(b);
 }
+
+
+template<typename T, typename B>
+const typename BufferTraits<T>::BCType& buffer(const B &b)
+{
+  return traits::access<T, B>::buffer(b);
+}
+
+
+template<typename T, typename B>
+typename BufferTraits<T>::BCIterator& buffer_iterator(const B &b)
+{
+  return traits::access<T, B>::buffer_iterator(b);
+}
+
+
+template<typename O>
+typename OBuffer<O>::BCSizes& sizes(OBuffer<O> &b)
+{
+  return b.bc_sizes;
+}
+
 
 template<typename B, typename D>
 B& operator << (B &b, const std::vector<D> &v)
 {
   auto &bc = buffer<D>(b);
+  auto &bs = sizes(b);
+  bs.push_back(v.size());
   bc.insert(bc.end(), v.begin(), v.end());
   return b;
 }
 
 
-/// users implement this
-struct OtherType
+template<typename B, typename D>
+const B& operator >> (const B &b, std::vector<D> &v)
 {
-  std::vector<int> data;
-};
+  auto &bc = buffer<D>(b);
+  /*
+  auto &bs = sizes(b);
+  bs.push_back(v.size());
+  bc.insert(bc.end(), v.begin(), v.end());
+  */
+  return b;
+}
+
+
+template<typename B>
+void init_load(const B& b)
+{
+  auto &bit = buffer_iterator<int>(b);
+  bit = buffer<int>(b).begin(); 
+}
+
+
+
+
+/////////////////////////////////////////////////////
+///                   USER                       ////
+/////////////////////////////////////////////////////
 
 struct SomeType
 {
@@ -57,30 +123,40 @@ struct SomeType
 };
 
 template<typename Buffer>
-void serialize(Buffer &b, const SomeType &d)
+void save(Buffer &b, const SomeType &d)
 {
   b << d.data;
 }
 
 template<typename Buffer>
-void serialize(Buffer &b, const OtherType &d)
+void load(const Buffer &b, SomeType &d)
 {
+  b >> d.data;
 }
 
+
+int main()
+{
+  SomeType tput;
+  tput.data.insert(tput.data.begin(), 10, -1);
+  OBuffer<SomeType> b;
+  save(b, tput);
+  SomeType tget;
+  init_load(b);
+  load(b, tget);
+  return 0;
+}
+
+
+
+/*
 template<typename InputIt, typename OutputIt>
 OutputIt mpi_dummy(InputIt first, InputIt last, OutputIt result)
 {
   while(first != last)
-    {
-      first++; result++;
-    }
+  {
+    first++; result++;
+  }
   return result;
 }
- 
-int main()
-{
-  SomeType t;
-  OBuffer<SomeType> b;
-  serialize(b, t);
-  return 0;
-}
+ */
